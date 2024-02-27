aec391d2dda10adde3b52f41a506a00fc9201e69
Jezuz <Jezuz@DESKTOP-RN4DP0B>
2024-02-26 22:28:13 -0600

    Agregar archivos de proyecto.

---
+++ casas/Casas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casas
+{
+    internal class Casas
+    {
+        public string Casa {  get; set; }
+        public int Numero { get; set; }
+
+        public Casas()
+        {
+            Casa = " sin nombre";
+            Numero = 0;
+        }
+        
+        public Casas(string casas)
+        {
+            Casa = casas;
+            Numero = 0;
+        }
+
+        public Casas(string casas, int numero)
+        {
+            Casa= casas;
+            Numero = numero;
+        }
+        ~Casas()
+        {
+            Console.WriteLine($"casa con numero\"{Numero}\"a sido destruida");
+        }
+
+        public void SHOW()
+        {
+            Console.WriteLine("NUMERO"+Numero);
+            Console.Write("Casa a sido destrida"+Casa+"\n\n");
+        }
+
+    }
+}
---
+++ casas/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace casas
+{
+    internal class program
+    {
+        public static void Main(string[] args)
+        {
+            Casas lugar = new Casas();
+            Casas lugar1 = new Casas(" de asturias");
+            Casas lugar2 = new Casas(" de asturias",141);
+            Console.WriteLine(lugar.Casa + "-" + lugar.Numero);
+            Console.WriteLine(lugar1.Casa + "-" + lugar1.Numero);
+            Console.WriteLine(lugar2.Casa + "-" + lugar2.Numero);
+            lugar.SHOW();
+            lugar1.SHOW();
+            lugar2.SHOW();
+        }
+
+    }
+}
